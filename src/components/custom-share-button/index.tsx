ffc2a3c0f6a468a1afe1cbe729c616f3fdec6673
Neşathan Öztürk <107864503+nesathanozturk@users.noreply.github.com>
2024-10-14 21:11:18 +0300

    feat(custom-share-button): add gradient colors

--- src/components/custom-share-button/index.tsx
+++ src/components/custom-share-button/index.tsx
@@ -7,9 +7,14 @@ type CustomShareButtonProps = PropsWithChildren & {
     onClick?: () => void
 }
 
+const INSTAGRAM_GRADIENT = 'bg-gradient-to-r from-[#833ab4] via-[#fd1d1d] to-[#fcb045]'
+const LOCATION_GRADIENT = 'bg-gradient-to-r from-[#f12711] to-[#f5af19]'
+
 function CustomShareButton({ children, platform, onClick }: CustomShareButtonProps) {
+    const backgroundColor = platform === "instagram" ? `${INSTAGRAM_GRADIENT} hover:${INSTAGRAM_GRADIENT}` : `${LOCATION_GRADIENT} hover:${LOCATION_GRADIENT}`
+
     return (
-        <Button className={`rounded-full w-10 h-10 p-[6px] ${platform === "instagram" ? "bg-gradient-to-r from-[#833ab4] via-[#fd1d1d] to-[#fcb045]" : 'bg-orange-500'}`} onClick={onClick}>
+        <Button className={`rounded-full w-10 h-10 p-[6px] ${backgroundColor}`} onClick={onClick}>
             {children}
         </Button>
     )
